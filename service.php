fd11044d60544d121857335e7b42ac31999f2115
Gallinelli Martin <gallinellim@spa1.gba.gov.ar>
2021-07-05 12:12:21 -0300

    update cambio if por switch en service.php

--- service.php
+++ service.php
@@ -4,11 +4,107 @@ require_once 'classes/Curso.php';
 
 // $_SERVER['REQUEST_METHOD'] = averiguar el metodo con el que se solicito este documento
 
+// analizar request method
+switch ($_SERVER['REQUEST_METHOD']) {
+    /**
+     * ? GET
+     * ? se debe enviar por url el id del post [id] o nada
+     */
+    case 'GET':
+        //* si existe id en url (listar un curso)
+        if(isset($_GET['id']))
+        {
+            //* obtener el curso segun el id
+            $curso = Curso::obtenerCurso($_GET['id']);
+            // indicar tipo de respuesta en el header
+            header("Content-Type: application/json");
+            // si existe un error (400-500) o no (200)
+            isset($curso['success']) ? http_response_code($curso['success']) : http_response_code(200);
+            // convertir a json e imprimir
+            echo json_encode($curso);
+        //* si no existe id en url (listar todos los cursos)
+        } else {
+            //* obtener todos los cursos
+            $cursos = Curso::obtenerCursos();
+            // indicar tipo de respuesta en el header
+            header("Content-Type: application/json");
+            // si existe un error (400-500) o no (200)
+            isset($cursos['success']) ? http_response_code($cursos['success']) : http_response_code(200);
+            // convertir a json e imprimir
+            echo json_encode($cursos);
+        }
+        break;
+
+    /**
+     * ? POST
+     * ? se debe enviar en el body de la respuesta los campos a guardar
+     */    
+    case 'POST':
+        // capturar el nombre
+        $nombre = isset($_POST['nombre']) ? $_POST['nombre'] : '';
+        //* guardar el curso
+        $response = Curso::nuevoCurso($nombre);
+        // indicar tipo de respuesta en el header
+        header('Content-Type: application/json');
+        // si existe un error (400-500) o no (201)
+        http_response_code($response['success']);
+        // convertir a json e imprimir
+        echo json_encode($response);
+        break;
+
+    /**
+     * ? PUT
+     * ? se debe enviar en el body de la respuesta los campos a actualizar y el id del registro a actualizar
+     */
+    case 'PUT':
+        // capturar datos recibidos
+        $datos = file_get_contents("php://input");
+        $id = isset($_GET['id']) ? $_GET['id'] : '';
+        //* actualizar el curso
+        $response = Curso::actualizarCurso($datos, $id);
+        // indicar tipo de respuesta en el header 
+        header('Content-Type: application/json');
+        // si existe un error (400-500) o no (200)
+        http_response_code($response['success']);
+        // convertir array a string
+        echo json_encode($response);
+        break;
+
+    /**
+     * ? DELETE
+     * ? se debe enviar por url el id del post [id]
+     */
+    case 'DELETE':
+        // capturar el id recibido
+        $id = isset($_GET['id']) ? $_GET['id'] : '';
+        //* eliminar el curso
+        $response = Curso::eliminarCurso($id);
+        // indicar tipo de respuesta en el header 
+        header('Content-Type: application/json');
+        // si existe un error (400-500) o no (200)
+        http_response_code($response['success']);
+        // convertir array a string
+        echo json_encode($response);
+        break;
+    
+    /**
+     * ? OTRO CASO
+     */
+    default:
+        // indicar tipo de respuesta en el header
+        header('Content-Type: application/json');
+        // error 405 metodo no permitido
+        $response = Answers::mensaje('405', 'Método no permitido');
+        // convertir array a string
+        echo json_encode($response);
+        break;
+}
+
 /**
  * ? GET
  * ? se debe enviar por url el id del post [id] o nada
  */
-if ($_SERVER['REQUEST_METHOD'] == 'GET')
+/* if ($_SERVER['REQUEST_METHOD'] == 'GET')
 {
     //* si existe id en url (listar un curso)
     if(isset($_GET['id']))
@@ -31,13 +127,13 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET')
         isset($cursos['success']) ? http_response_code($cursos['success']) : http_response_code(200);
         // convertir a json e imprimir
         echo json_encode($cursos);
-    }
+    } */
 
 /**
  * ? POST
  * ? se debe enviar en el body de la respuesta los campos a guardar
  */
-} elseif($_SERVER['REQUEST_METHOD'] == "POST")
+/* } elseif($_SERVER['REQUEST_METHOD'] == "POST")
 {
     // capturar el nombre
     $nombre = isset($_POST['nombre']) ? $_POST['nombre'] : '';
@@ -48,13 +144,13 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET')
     // si existe un error (400-500) o no (201)
     http_response_code($response['success']);
     // convertir a json e imprimir
-    echo json_encode($response); 
+    echo json_encode($response);  */
 
 /**
  * ? PUT
  * ? se debe enviar en el body de la respuesta los campos a actualizar y el id del registro a actualizar
  */
-} elseif($_SERVER['REQUEST_METHOD'] == "PUT")
+/* } elseif($_SERVER['REQUEST_METHOD'] == "PUT")
 {
     // capturar datos recibidos
     $datos = file_get_contents("php://input");
@@ -66,13 +162,13 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET')
     // si existe un error (400-500) o no (200)
     http_response_code($response['success']);
     // convertir array a string
-    echo json_encode($response);
+    echo json_encode($response); */
 
 /**
  * ? DELETE
  * ? se debe enviar por url el id del post [id]
  */
-} elseif($_SERVER['REQUEST_METHOD'] == "DELETE")
+/* } elseif($_SERVER['REQUEST_METHOD'] == "DELETE")
 {
     // capturar el id recibido
     $id = isset($_GET['id']) ? $_GET['id'] : '';
@@ -83,16 +179,16 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET')
     // si existe un error (400-500) o no (200)
     http_response_code($response['success']);
     // convertir array a string
-    echo json_encode($response);
+    echo json_encode($response); */
 
 /**
  * ? OTRO CASO
  */
-} else {
+/* } else {
     // indicar tipo de respuesta en el header
     header('Content-Type: application/json');
     // error 405 metodo no permitido
     $response = Answers::mensaje('405', 'Método no permitido');
     // convertir array a string
     echo json_encode($response);
-}
+} */
